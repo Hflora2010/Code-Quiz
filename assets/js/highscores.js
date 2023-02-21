acffe46b17606273c998c52541d7880e54547d3c
Hailey Flora <Haileycflora@gmail.com>
2023-02-21 16:55:40 -0500

    add comment to highscores.js

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,6 +1,7 @@
 
 var scores = JSON.parse(localStorage.getItem("score")) || [];
 
+// Getting locally stored highscores and showing them in a list on highscores page.
     for (let i = 0; i < scores.length; i++) {
     var li = document.createElement("li");
     li.textContent = scores[i];
